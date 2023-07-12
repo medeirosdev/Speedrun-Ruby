58d8aa30f05fb0439df405fed107c37c79d95000
medeirosdev <guilherme.ggcm20@gmail.com>
2023-07-12 20:59:19 -0300

    :sparkles: estudando hashes

---
+++ hashes.rb
@@ -0,0 +1,30 @@
+## Hashes
+
+## Vamos criar um Hashe
+
+exampleHash = {
+	"name" => "gui",
+	"age" => 23,
+	"cpf" => 243-234-642-32,
+	"array" => [2,45,23]
+}
+
+## Removendo data
+
+
+
+
+if __FILE__ == $0
+
+	puts exampleHash["name"]
+	puts exampleHash["age"]
+	puts exampleHash["age"] + 23
+	
+	puts exampleHash
+	
+	exampleHash.delete("cpf")
+	
+	
+	puts exampleHash
+	
+end
